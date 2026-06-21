dbf3ea75b037cf6051246246d489c07850133c7b
trekswithnick <nicholasbrowningdev@outlook.com>
2026-06-21 17:24:47 +1000

    Initialize repo and add types

---
+++ src/types.ts
@@ -0,0 +1,58 @@
+interface BaseMonitor {
+    
+};
+
+interface QuarterbackMonitor {
+    returnSuperBowlLossesCalculation(numberOfLosses: number): number;
+    returnPassingYardsCalculation(debutYear: string, passingYards: number): number;
+    returnPassingTouchdownsCalculation(debutYear: string, passingYards: number): number;
+    returnNoChampionshipsCalculation(): number;
+};
+
+interface RunningBackMonitor {
+
+}
+
+interface WideReceiverMonitor {
+
+}
+
+interface TightEndMonitor {
+
+}
+
+interface OffensiveTackleAndGuardMonitor {
+
+}
+
+interface CenterMonitor {
+
+}
+
+interface InsideLinebackerMonitor {
+
+}
+
+interface OutsideLinebackerMonitor {
+
+}
+
+interface DefensiveBackMonitor {
+
+}
+
+interface DefensiveEndMonitor {
+
+}
+
+interface DefensiveTackleMonitor {
+
+}
+
+interface KickerMonitor {
+
+}
+
+interface PunterMonitor {
+
+}
